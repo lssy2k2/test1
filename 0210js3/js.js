a996f5961866b0275652229f28f49ec88490a8cc
lssy2k1 <lssy2k1@gmail.com>
2023-02-10 17:29:52 +0900

    0219 greeting

---
+++ 0210js3/js.js
@@ -0,0 +1,20 @@
+/* function add (a, b){
+    console.log ( a + b );
+    return a + b;  //저장해서 반환
+    }
+    
+    var result = add (3, 4);
+    console.log("두 수를 더한 값 : " + result)
+
+
+    const a = 100;
+    function sample(a){
+    if ( a > 0 ) {
+    return;
+    }
+    console.log('hello world')//내부 함수가 맞으면 콘솔로그 헬로월드를 반환해달라.
+    }
+
+    sample();  */
+
+    
